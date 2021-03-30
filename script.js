fc8393c97f7647536241647fe863a35b9588bc68
hamza-ouni <hamza-ouni@outlook.com>
2021-03-30 01:24:26 +0100

    Styling button

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ let bodyColor = document.querySelector("body");
 let button = document.querySelector("#btn");
 let paragraph = document.getElementsByTagName("p");
 let header = document.getElementsByClassName("header");
+// on click button
 button.addEventListener("click", () => {
+  //Change background color of the body
   bodyColor.style.background = "magenta";
+  // Iterate through the array containing all the p tag of the HTML
   for (let i = 0; i < paragraph.length; i++) {
+    // Change the color of all ements
     paragraph[i].style.color = "blue";
+    // Change the font style of all elements of the array
     paragraph[i].style.fontFamily = "courier";
   }
+  // Iterate through the array containing elements with class header
   for (let i = 0; i < header.length; i++) {
+    // Change the color of all ements
     header[i].style.color = "green";
+    // Change the font style of all elements of the array
     header[i].style.fontFamily = "Comic Sans, sans-serif";
   }
 });
